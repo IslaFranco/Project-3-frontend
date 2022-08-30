394c723dcf3bfc263fd516adbf838dff5f5967e2
islafranco <isla_franco@icloud.com>
2022-08-29 19:43:03 -0700

    stone data is connected with front end

--- src/components/StoneMain.js
+++ src/components/StoneMain.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import StoneIndex from '../pages/StoneIndex';
 import StoneShow from '../pages/StoneShow';
 
 function StoneMain(props) {
+    const [ stone, setStone ] = useState(null)
+
+    const API_URL = 'http://localhost:3001/stones/seed';
+
+    const getStone = async () => {
+        try {
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            setStone(data);
+        } catch (error) {
+            // to do: add logic or task
+        }
+    };
+
+    useEffect(() => {
+        getStone();
+    }, [])
+
     return (
     <main>
         <Routes>
-           <Route path="/" element={<StoneIndex />} />
+           <Route path="/" element={<StoneIndex stone={stone} />} />
            <Route path="/stones/:id" element={<StoneShow />} />
         </Routes>
     </main>
--- src/pages/StoneIndex.js
+++ src/pages/StoneIndex.js
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+
 function StoneIndex(props) {
-    return <h1>Stone index page component</h1>
+
+    const loaded = () => {
+        return props.stone.map(({ name, description, img, color, _id })=> {
+            return (
+                <div className="stone" key={_id}>
+                    <Link to={`/stones/{$_id}`}>
+                       <h1>{name}</h1>
+                    </Link>
+                    <img src={img} alt={name} />
+                    <h3>{description}</h3>
+                    <h3>{color}</h3>
+                </div>
+            )
+        })
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    };
+
+
+    return props.stone ? loaded() : loading();
+
+
+
+    // return <h1>Stone index page component</h1>
 }
 
 export default StoneIndex;
